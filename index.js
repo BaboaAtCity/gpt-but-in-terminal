08348e6ad0f6037fa9a5b7a5487b878bc272ad01
BaboaAtCity <Mohammad.baboa@city.ac.uk>
2024-05-21 19:30:05 +0100

    improved chat loop

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const OpenAI = require("openai");
 const dotenv = require("dotenv");
 const inquirer = require('inquirer');
 const readline = require('readline');
+const chalk = require('chalk');
 
 
 dotenv.config();
@@ -9,43 +10,33 @@ const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
-async function question(msg, model_name) {
+async function question(message, model) {
   const completion = await openai.chat.completions.create({
     messages: [{
         role: "system",
-        content: "You are a helpful assistant. keep your answers short and without markdown in plain text as this is part of a chat application"
+        content: "You are a helpful assistant. keep your answers brief and without markdown in plain text as this is part of a chat application"
       },
       {
         role: "user",
-        content: msg
+        content: message
       }
     ],
-    max_tokens: 60,
-    model: model_name,
+    max_tokens: 200,
+    model: model,
   });
   const response = completion.choices[0].message.content;
   return response;
 }
 
-async function prompt() {
-  const models = ["gpt-3.5-turbo", "gpt-4-turbo", "gpt-4o"];
-
-  const userAnswers = await inquirer
+async function messagePrompt() {
+  const userMessage = await inquirer
     .prompt([{
-        name: "model_name",
-        type: "list",
-        message: "Which GPT model would you like to use?",
-        choices: models,
-        default: "gpt-3.5-turbo",
-      },
-      {
-        name: "message",
-        type: "input",
-        message: "Enter you message to GPT",
-      },
-    ])
-    .then((answer) => {
-      return answer;
+      name: "message",
+      type: "input",
+      message: "Enter you message to GPT (Press ESC to quit)",
+    }, ])
+    .then((userMessage) => {
+      return userMessage;
     })
     .catch((error) => {
       if (error.isTtyError) {
@@ -54,9 +45,35 @@ async function prompt() {
         console.error(error);
       }
     });
-  return userAnswers;
+
+  return userMessage;
+}
+
+async function modelPrompt() {
+  const models = ["gpt-3.5-turbo", "gpt-4-turbo", "gpt-4o"];
+
+  const model = await inquirer
+    .prompt([{
+      name: "model_name",
+      type: "list",
+      message: "Which GPT model would you like to use?",
+      choices: models,
+      default: "gpt-3.5-turbo",
+    }])
+    .then((model) => {
+      return model;
+    })
+    .catch((error) => {
+      if (error.isTtyError) {
+        console.error(error);
+      } else {
+        console.error(error);
+      }
+    });
+  return model;
 }
 async function main() {
+  tokens = 0;
   const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
@@ -71,11 +88,13 @@ async function main() {
   }
 
   rl.input.on('keypress', onKeyPress);
-
+  const modelObj = await modelPrompt();
   while (true) {
-    const answers = await prompt();
-    const gptResponse = await question(answers.message, answers.model_name);
-    console.log(gptResponse);
+    const messageObj = await messagePrompt();
+    const gptResponse = await question(messageObj.message, modelObj.model_name);
+    console.log(chalk.blue(gptResponse));
+    tokens = tokens + 60
+    //console.log('tokens used estimate: ' + tokens)
   }
 }
 
